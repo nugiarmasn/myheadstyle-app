bcccd5fab8a02a9f101cd1d312f9f8b6e0d26581
nugiarmasn <nugiarmasnurizky@gmail.com>
2025-12-29 03:00:35 +0700

    flutter

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // Ini sudah benar di sini
+}
+
+android {
+    namespace =  "com.myheadstyle.app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    // BARIS YANG SALAH SUDAH DIHAPUS DARI SINI
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId =  "com.myheadstyle.app"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ app/src/main/kotlin/com/example/myheadsyle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myheadstyle.app // Pastikan ini sama persis dengan applicationId di Gradle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
